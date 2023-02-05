9c4383aca450a9cae7a7091e05d7920c6c752c0b
Maksim Nezhinskii <maksimnezhin@gmail.com>
2023-02-05 15:22:26 +0300

    Added interface for folder and several image processing

--- parking_occupancy.py
+++ parking_occupancy.py
@@ -17,24 +17,12 @@ class ParkingPlace:
         bbPath = mplPath.Path([point for point in zip(self.x_coords, self.y_coords)])
         return bbPath.contains_point((x, y))
 
-def show_occupancy(labels_path, image_path):
-    parking_places = []
-    with open(labels_path) as file:
-        labels = csv.reader(file, delimiter = ",")
-        for row in labels:
-            coords = re.findall(r'(?<=\[)(?:\d+,?)+(?=\])',row[5])
-            if len(coords) == 2:
-                parking_places.append(ParkingPlace([int(x) for x in coords[0].split(',')], [int(y) for y in coords[1].split(',')]))
-
+def image_processing(parking_places, image_path):
     image = Image.open(image_path)
     draw = ImageDraw.Draw(image, 'RGBA') 
 
     model = torch.hub.load('ultralytics/yolov5', 'yolov5m')
     results = model(image_path).pandas().xyxy[0].values
-
-    for bb in results:
-        if bb[5] == 2 or bb[5] == 7:
-            draw.rectangle(xy = [(bb[0], bb[1]), (bb[2], bb[3])], outline = (255, 255, 0), width = 3)
     
     for place in parking_places:
         flag = False
@@ -47,6 +35,36 @@ def show_occupancy(labels_path, image_path):
         else:
             draw.polygon(xy = [point for point in zip(place.x_coords, place.y_coords)], fill = (0, 255, 0, 40), outline = (0, 255, 0), width = 2)
 
+    for bb in results:
+        if bb[5] == 2 or bb[5] == 7:
+            draw.rectangle(xy = [(bb[0], bb[1]), (bb[2], bb[3])], outline = (255, 255, 0), width = 3)
+    
     path = Path(image_path)   
     image.save("{0}/{1}_processed{2}".format(path.parent, path.stem, path.suffix))
     return image
+
+def analyze_parking_spaces(labels, source):
+    parking_places = []
+    with open(labels) as file:
+        labels = csv.reader(file, delimiter = ",")
+        for row in labels:
+            coords = re.findall(r'(?<=\[)(?:\d+,?)+(?=\])',row[5])
+            if len(coords) == 2:
+                parking_places.append(ParkingPlace([int(x) for x in coords[0].split(',')], [int(y) for y in coords[1].split(',')]))
+    
+    images = []
+    if isinstance(source, list):
+        images = source
+    else:
+        source = Path(source)
+        if source.is_dir():
+            images = [str(image) for image in source.glob('**/*.jpg')]
+        else:
+            images = [str(source)]
+
+    for image in images:
+        image_processing(parking_places, image)
+
+
+analyze_parking_spaces(labels = 'examples/several_images/parking_places.csv', 
+    source = ['examples/several_images/2015-12-18_0748.jpg', 'examples/several_images/2015-12-18_0948.jpg'])
