2497e8f46039fbc922f32a02491a212fe12c6a79
nezhinskii <maksimnezhin@gmail.com>
2023-02-27 17:38:59 +0300

    Added inference

--- app.py
+++ app.py
@@ -3,28 +3,45 @@ import detection.parking_occupancy as po
 import pandas as pd
 import cv2
 import numpy as np
+import torch
 
-parking_occupancy = po.ParkingOccupancy()
+@st.cache_data(show_spinner=False)
+def load_model():
+    return po.ParkingOccupancy(torch.hub.load('ultralytics/yolov5', 'custom', path='detection\coco-voc.pt', verbose=False, force_reload=True))
 
-st.set_page_config(layout="wide")
-st.title("THE PROBLEM OF MONITORING THE CONGESTION OF PARKING SPACES")
-uploaded_csv = st.file_uploader("Upload parking spaces annotation", type = [".csv", ".txt"])
-if uploaded_csv is not None:
-    parking_occupancy.load_spaces_annotation(pd.read_csv(uploaded_csv))
+def upload_source():
+    uploaded_csv = st.file_uploader("Upload parking spaces annotation", type = [".csv", ".txt"])
+    if uploaded_csv is not None:
+        parking_occupancy.load_spaces_annotation(pd.read_csv(uploaded_csv))
+    
+    uploaded_source = st.file_uploader("Upload source file", type = [".img", ".jpg", ".png"])
+    if uploaded_source is not None:
+        file_bytes = np.asarray(bytearray(uploaded_source.read()), dtype=np.uint8)
+        opencv_image = cv2.imdecode(file_bytes, 1)
+        parking_occupancy.load_image_source(opencv_image)
+    return uploaded_csv is not None and uploaded_source is not None
 
-uploaded_source = st.file_uploader("Upload source file", type = [".img", ".jpg", ".png"])
-if uploaded_source is not None:
-    file_bytes = np.asarray(bytearray(uploaded_source.read()), dtype=np.uint8)
-    opencv_image = cv2.imdecode(file_bytes, 1)
-    parking_occupancy.load_image_source(opencv_image)
-
-col1, col2 = st.columns([0.5, 0.5])
-with col1:
-    st.subheader("Preview")
-    if uploaded_source is not None and uploaded_csv is not None:
+def show_preview(preview_col):
+    with preview_col:
+        st.markdown("<h2 style='text-align: center;'>Preview</h1>", unsafe_allow_html=True)
         parking_occupancy.create_prewiew()
         st.image(parking_occupancy.preview, channels="BGR")
-    else:
-        st.empty()
-with col2:
-    st.subheader("Result")
+
+def show_result(result_col):
+    with result_col:
+        st.markdown("<h2 style='text-align: center;'>Result</h1>", unsafe_allow_html=True)
+        with st.spinner('Wait for the model to make inference'):
+            result = parking_occupancy.process_frame()
+        st.image(result, channels="BGR")
+
+
+st.set_page_config(layout="wide", page_title="Parking spaces occupancy",)
+st.title("THE PROBLEM OF MONITORING THE CONGESTION OF PARKING SPACES")
+with st.spinner('Wait for the model to load'):
+    parking_occupancy = load_model()
+is_ready = upload_source()
+
+preview_col, result_col = st.columns(2)
+if is_ready:
+    show_preview(preview_col)
+    show_result(result_col)
--- detection/parking_occupancy.py
+++ detection/parking_occupancy.py
@@ -1,11 +1,7 @@
-import csv
 import re
 import matplotlib.path as mplPath
-import torch
 import cv2
 import numpy as np
-from pathlib import Path
-from tqdm import tqdm
 
 class ParkingPlace:
 
@@ -21,9 +17,14 @@ class ParkingPlace:
         return bbPath.contains_point((x, y))
 
 class ParkingOccupancy:
+    LINE_THIKNESS = 3
+    FREE_COLOR = (0, 255, 0)
+    BUSY_COLOR = (0, 0, 255)
+    DEFAULT_COLOR = (255,255,255)
+    BB_COLOR = (255, 0, 255)
 
-    def __init__(self):
-        model = torch.hub.load('ultralytics/yolov5', 'custom', path='../weights/coco-voc.pt', verbose = False, force_reload=True)
+    def __init__(self, model):
+        self.model = model
 
     def load_spaces_annotation(self, labels):
         self.parking_places = []
@@ -43,7 +44,25 @@ class ParkingOccupancy:
     def create_prewiew(self):
         self.spaces_mask = np.zeros((self.image_source.shape[0], self.image_source.shape[1], 3), dtype=np.uint8)
         for place in self.parking_places:
-            color = (255,255,255)
-            cv2.polylines(self.spaces_mask, [np.column_stack((place.x_coords, place.y_coords))], True, color, 2)
-        self.preview = cv2.addWeighted(self.image_source, 1, self.spaces_mask, 1, 0.6)
+            cv2.polylines(self.spaces_mask, [np.column_stack((place.x_coords, place.y_coords))], True, self.DEFAULT_COLOR, self.LINE_THIKNESS)
+        self.preview = cv2.addWeighted(self.image_source, 1, self.spaces_mask, 1, 0)
 
+    def process_frame(self):
+        results = self.model(self.image_source).pandas().xyxy[0].values
+        result_mask = np.zeros((self.image_source.shape[0], self.image_source.shape[1], 3), dtype=np.uint8)
+        for place in self.parking_places:
+            flag = False
+            for bb in results:
+                if (place.contains_point(bb[0] + (bb[2] - bb[0]) / 2, bb[1] + (bb[3] - bb[1]) * 3/4)):
+                    flag = True
+                    break
+            if flag:
+                color = self.BUSY_COLOR
+            else:
+                color = self.FREE_COLOR
+            cv2.polylines(result_mask, [np.column_stack((place.x_coords, place.y_coords))], True, color, self.LINE_THIKNESS)
+
+        for bb in results:
+            cv2.rectangle(result_mask, (int(bb[0]), int(bb[1])), (int(bb[2]), int(bb[3])), self.BB_COLOR, self.LINE_THIKNESS)
+
+        return cv2.addWeighted(self.image_source, 1, result_mask, 1, 0.0)
