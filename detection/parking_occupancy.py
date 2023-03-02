d151abe89bcf161880111c058d6976ae7b7ec8cf
nezhinskii <maksimnezhin@gmail.com>
2023-03-02 16:35:39 +0300

    Added confidence treshold

--- detection/parking_occupancy.py
+++ detection/parking_occupancy.py
@@ -22,6 +22,7 @@ class ParkingOccupancy:
     BUSY_COLOR = (0, 0, 255)
     DEFAULT_COLOR = (255,255,255)
     BB_COLOR = (255, 0, 255)
+    CONFIDENCE_TRESHOLD = 0.5
 
     def __init__(self, model):
         self.model = model
@@ -47,13 +48,16 @@ class ParkingOccupancy:
             cv2.polylines(self.spaces_mask, [np.column_stack((place.x_coords, place.y_coords))], True, self.DEFAULT_COLOR, self.LINE_THIKNESS)
         self.preview = cv2.addWeighted(self.image_source, 1, self.spaces_mask, 1, 0)
 
+    def __place_busy(sself, place, bb):
+        return place.contains_point(bb['xmin'] + (bb['xmax'] - bb['xmin']) / 2, bb['ymin'] + (bb['ymax'] - bb['ymin']) * 3/4)
+    
     def process_frame(self):
-        results = self.model(self.image_source).pandas().xyxy[0].values
+        results = self.model(self.image_source).pandas().xyxy[0]
         result_mask = np.zeros((self.image_source.shape[0], self.image_source.shape[1], 3), dtype=np.uint8)
         for place in self.parking_places:
             flag = False
-            for bb in results:
-                if (place.contains_point(bb[0] + (bb[2] - bb[0]) / 2, bb[1] + (bb[3] - bb[1]) * 3/4)):
+            for _, bb in results.iterrows():
+                if bb['confidence'] > self.CONFIDENCE_TRESHOLD and self.__place_busy(place, bb):
                     flag = True
                     break
             if flag:
@@ -62,7 +66,8 @@ class ParkingOccupancy:
                 color = self.FREE_COLOR
             cv2.polylines(result_mask, [np.column_stack((place.x_coords, place.y_coords))], True, color, self.LINE_THIKNESS)
 
-        for bb in results:
-            cv2.rectangle(result_mask, (int(bb[0]), int(bb[1])), (int(bb[2]), int(bb[3])), self.BB_COLOR, self.LINE_THIKNESS)
+        for _, bb in results.iterrows():
+            if bb['confidence'] > self.CONFIDENCE_TRESHOLD:
+                cv2.rectangle(result_mask, (int(bb['xmin']), int(bb['ymin'])), (int(bb['xmax']), int(bb['ymax'])), self.BB_COLOR, self.LINE_THIKNESS)
 
         return cv2.addWeighted(self.image_source, 1, result_mask, 1, 0.0)
