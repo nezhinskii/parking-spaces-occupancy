9f2dcce98df498da341bd5b7121c2dd83d1886d5
nezhinskii <maksimnezhin@gmail.com>
2023-03-04 17:42:59 +0300

    Fixed model caching

--- app.py
+++ app.py
@@ -5,18 +5,18 @@ import widgets.annotation_uploader as annotation_uploader
 import widgets.source_uploader as source_uploader
 import widgets.preview_result as preview_result
 
-@st.cache_data(show_spinner=False)
+@st.cache_resource(show_spinner=False)
 def load_model():
     return po.ParkingOccupancy(torch.hub.load('ultralytics/yolov5', 'custom', path='weights\coco-voc.pt', verbose=False, force_reload=True))
 
 st.set_page_config(layout="wide", page_title="Parking spaces occupancy",)
 st.title("THE PROBLEM OF MONITORING THE CONGESTION OF PARKING SPACES")
 annotation_ready = False
-#with st.spinner('Wait for the model to load'):
-parking_occupancy = load_model()
+with st.spinner('Wait for the model to load'):
+    parking_occupancy = load_model()
 annotation_ready = annotation_uploader.upload_annotation(parking_occupancy)
 source_ready = source_uploader.upload_source(parking_occupancy)
 if annotation_ready and source_ready:
     preview_col, result_col = st.columns(2)
-    preview_result.show_preview(preview_col)
-    preview_result.show_result(result_col)
+    preview_result.show_preview(parking_occupancy, preview_col)
+    preview_result.show_result(parking_occupancy, result_col)
